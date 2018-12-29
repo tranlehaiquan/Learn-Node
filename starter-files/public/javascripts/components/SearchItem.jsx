4ac8e920166ef202ed00f90a517411a0cca92712
Tran Le Hai Quan <tranlehaiquan@gmail.com>
2018-12-30 01:03:24 +0700

    Handle keyboard event arrowup, arrowdown, enter

--- starter-files/public/javascripts/components/SearchItem.jsx
+++ starter-files/public/javascripts/components/SearchItem.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import classnames from 'classnames';
 
 export default class SearchItem extends Component {
   static propTypes = {
-    item: propTypes.object.isRequired
+    item: propTypes.object.isRequired,
+    isSelect: propTypes.bool
   }
 
   static defaultProps = {
-    item: undefined
+    item: undefined,
+    isSelect: false
   }
-  
+
   render() {
-    const { item } = this.props;
+    const { item, isSelect } = this.props;
 
     return (
-      <a href={`/store/${item.slug}`} className="search__result">
+      <a href={`/store/${item.slug}`} className={classnames('search__result', isSelect && 'search__result--active')}>
         <strong>{item.name}</strong>
       </a>
     );
--- starter-files/public/javascripts/components/search.jsx
+++ starter-files/public/javascripts/components/search.jsx
@@ -9,7 +9,8 @@ class Search extends React.Component {
     
     this.state = {
       keyword: '',
-      stores: []
+      stores: [],
+      indexActive: -1
     };
 
     this.getStore = debounce(this.getStore, 300);
@@ -31,18 +32,51 @@ class Search extends React.Component {
 
   onChange = (e) => {
     this.setState({
-      keyword: e.target.value
+      keyword: e.target.value,
+      indexActive: -1
     });
 
     this.getStore();
   }
 
+  handleKeyUp = (e) => {
+    const { key } = e;
+    const { indexActive, stores } = this.state;
+
+    if( key !== 'ArrowUp' && key !== 'ArrowDown' && key !== 'Enter') return;
+    e.preventDefault();
+
+    if( key === 'ArrowUp' ) {
+      if( indexActive < 0 ) return;
+
+      this.setState(() => ({
+        indexActive: indexActive - 1
+      }));
+    }
+
+    if( key === 'ArrowDown' ) {
+      if( indexActive >= stores.length - 1 || !stores.length ) return;
+
+      this.setState(() => ({
+        indexActive: indexActive + 1
+      }));
+    }
+
+    if( key === 'Enter' ) {
+      if( indexActive < 0) return;
+      const { slug } = stores[indexActive];
+
+      window.location.pathname = `/store/${slug}`;
+    }
+  }
+
   render() {
-    const { keyword, stores } = this.state;
+    const { keyword, stores, indexActive } = this.state;
     return (
       <Fragment>
         <input
           onChange={this.onChange}
+          onKeyDown={this.handleKeyUp}
           value={keyword} 
           className="search__input" 
           placeholder="Coffee, beer..." 
@@ -50,8 +84,12 @@ class Search extends React.Component {
         />
         <div className="search__results">
           {
-            stores.map((store) => (
-              <SearchItem key={store.slug} item={store} />
+            stores.map((store, index) => (
+              <SearchItem 
+                key={store.slug} 
+                item={store}
+                isSelect={index === indexActive} 
+              />
             ))
           }
         </div>
